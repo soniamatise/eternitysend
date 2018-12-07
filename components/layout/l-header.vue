9ae65146fa6cac9d48973977b9a3eacd7001a994
Sonia <sonia@matise.nl>
2018-12-07 19:25:54 +0100

    progress

---
+++ components/layout/l-header.vue
@@ -0,0 +1,17 @@
+<template>
+	<header id="header" class="header">
+		<h3 />
+	</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '~tools';
+
+.header {
+	// Header styles
+}
+</style>
